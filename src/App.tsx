32a1ce884cffd58b167dca8bf17cf4c39bc1c123
Somoy khan <k.somoykhan@gmail.com>
2020-02-01 14:16:46 +0600

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+
+const useInput = (initialValue: number) => {
+  const [inputvalue, setInputvalue] = useState(initialValue);
+  return {
+    inputvalue,
+    setInputvalue,
+    bind: {
+      inputvalue,
+      onChange: (event: any) => {
+        setInputvalue(event.target.value);
+      }
+    }
+  };
+};
+const factorial = (n: number): number => {
+  if (n == 0 || n == 1){
+    return 1;
+  }else {
+    return factorial(n-1) * n
+  }
+} 
 
 const App = () => {
+  const { inputvalue, bind } = useInput(0);
+  const [value, setValue] = useState(0);
+
+
+  const handleSubmit = (evt: any) => {
+    evt.preventDefault();
+    setValue(factorial(inputvalue));
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Factorial Calculator</h1>
+      <form onSubmit={handleSubmit}>
+        <input 
+        {...bind}
+        type="number" 
+        defaultValue="0"
+        placeholder="Enter a number..." />
+        <br />
+        <button>Calculate Factorial</button>
+      </form>
+      <h2>Factorial: {value}</h2>
     </div>
   );
-}
+};
 
 export default App;
